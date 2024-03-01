ecad1a9cf53a037f0bc4d6b4624a4ca712ebca29
matinsajadii <emtinsajadi79@gmail.com>
2024-03-01 14:00:25 +0330

    update training code with mongard

--- celery/one.py
+++ celery/one.py
@@ -7,13 +7,25 @@ app = Celery(main="one", broker="amqp://guest:guest@localhost:5672", backend="rp
 logger = get_task_logger(__name__)
 
 
+# app.config_from_object("celery_conf")
+app.conf.update(
+    task_time_limit = 60,
+    task_soft_time_limit = 50,
+    worker_concurency = 70,
+    worker_prefetch_multiplier = 0,
+    task_ignore_result = True,
+)
 
-# @app.task(name="app.adding")
-# def add(a, b):
-#     time.sleep(15)
-#     return a + b 
 
 #########################
+
+@app.task(name="app.adding")
+def add(a, b):
+    time.sleep(3)
+    return a + b 
+
+#########################
+
 
 @app.task(bind=True, default_retry_delay=600)
 def devision(self, a, b):
@@ -24,3 +36,6 @@ def devision(self, a, b):
     except ZeroDivisionError:
         logger.info("Sorry...")
         return self.retry(countdown=10, max_retries=10)
+    
+
+result = add.apply_async((4, 5), link=devision.signature((2, 1)), imutable=True)
